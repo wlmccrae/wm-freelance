02a4438fa4277f30a349fb0d7617e1dd1ef1faa4
Wanda McCrae <wanda.mccrae@gmail.com>
2025-08-06 01:06:38 -0400

    Added FirstTime.jsx for special offer for 1st time customers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import Samples from './components/Samples';
 import Contact from './components/Contact';
 import Thanks from './components/Thanks';
+import FirstTime from './components/FirstTime';
 import './App.css';
 import './styles/global.css';
 
@@ -21,6 +22,7 @@ function App() {
             <Route path="samples" element={<Samples />} />
             <Route path="contact" element={<Contact />} />
             <Route path="thanks" element={<Thanks />} />
+            <Route path="first-time" element={<FirstTime />} />
           </Routes>
         </div>
         <Footer />
---
+++ src/components/FirstTime.js
@@ -0,0 +1,18 @@
+import '../styles/FirstTime.css';
+
+function FirstTime () {
+  return (
+    <div className="special-offer">
+      <h1>Special Offer for First Time Customers</h1>
+      <h2>Mobile-friendly website</h2>
+      <h2 className='bullet'> - (3-5 pages)</h2>
+      <h2 className='bullet'> - quick turnaround</h2>
+      <h2 className='bullet'> - includes contact form</h2>
+      <h2 className='bullet'> - Includes Google Maps pin</h2>
+      <h1>Starting at $499USD.</h1>
+      <p>Offer expires 30 September 2025.</p>
+    </div>
+  );
+};
+
+export default FirstTime;
