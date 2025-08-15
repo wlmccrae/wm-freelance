323ef2a915889bc4eab502ff9e7405b49c732588
Wanda McCrae <wanda.mccrae@gmail.com>
2025-08-15 16:14:56 -0400

    Styled first time offer page

--- src/components/FirstTime.js
+++ src/components/FirstTime.js
@@ -3,13 +3,15 @@ import '../styles/FirstTime.css';
 function FirstTime () {
   return (
     <div className="special-offer">
-      <h1>Special Offer for First Time Customers</h1>
-      <h2>Mobile-friendly website</h2>
-      <h2 className='bullet'> - (3-5 pages)</h2>
-      <h2 className='bullet'> - quick turnaround</h2>
-      <h2 className='bullet'> - includes contact form</h2>
-      <h2 className='bullet'> - Includes Google Maps pin</h2>
-      <h1>Starting at $499USD.</h1>
+      <h1>Special One Time Offer</h1>
+      <div className="offer-details">
+        <h2>New Mobile-Friendly Website</h2>
+        <h2 className='bullet'> - 1 or 2 pages</h2>
+        <h2 className='bullet'> - Quick turnaround</h2>
+        <h2 className='bullet'> - Includes contact form</h2>
+        <h2 className='bullet'> - Includes Google Map with pin</h2>
+      </div>
+      <h1>Starting at $299USD.</h1>
       <p>Offer expires 30 September 2025.</p>
     </div>
   );
