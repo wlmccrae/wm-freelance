de9fb005b45b9db04b00dcb3b7f2cb4528c6c29d
Wanda McCrae <wanda.mccrae@gmail.com>
2025-08-22 16:42:34 -0400

    Centered special offer information.

--- src/components/SpecialOffer.js
+++ src/components/SpecialOffer.js
@@ -4,12 +4,14 @@ function SpecialOffer() {
   return (
     <div className="special-offer">
       <h1>Special One Time Offer</h1>
-      <div className="offer-details">
-        <h2>New Mobile-Friendly Website</h2>
-        <h2 className='bullet'> - 1 or 2 pages</h2>
-        <h2 className='bullet'> - Quick turnaround</h2>
-        <h2 className='bullet'> - Includes contact form</h2>
-        <h2 className='bullet'> - Includes Google Map with pin</h2>
+      <div className="offer-container">
+        <div className="offer-details">
+          <h2>New Mobile-Friendly Website</h2>
+          <h2 className='bullet'> - 1 or 2 pages</h2>
+          <h2 className='bullet'> - Quick turnaround</h2>
+          <h2 className='bullet'> - Includes contact form</h2>
+          <h2 className='bullet'> - Includes Google Map with pin</h2>
+        </div>
       </div>
       <h1>Starting at $299USD.</h1>
       <p>Offer expires 30 September 2025.</p>
